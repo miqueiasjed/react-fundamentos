daffe72b43edc8f6290f1517cc77280f73165e8d
Miqueias Mesquita <miqueias_jed@hotmail.com>
2025-01-15 12:11:18 -0300

    mudanca de menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,4 +1,4 @@
-import { IconBraces, IconCode } from "@tabler/icons-react";
+import { IconBraces, IconCode, IconFileCheck, IconFunction, IconH1, IconSitemap } from "@tabler/icons-react";
 import MenuItem from "./menuItem";
 
 export default function Menu() {
@@ -7,10 +7,17 @@ export default function Menu() {
       flex flex-col justify-start w-72
       text-3xl p-2 gap-2
     `}>
-        <MenuItem icon={<IconCode />} texto='Basico' url="/components/basico"/>
-        <MenuItem icon={<IconCode />} texto='Título' url="/components/titulo"/>
-        <MenuItem icon={<IconCode />} texto='Página 1' url="/fundamentos/pagina"/>
-        <MenuItem icon={<IconBraces />} texto='Página 2' url="/pagina"/>
+        <span className="text-sm text-zinc-500 pl-3 pt-4">Estado</span>
+        <MenuItem icon={<IconCode />} texto='Componente sem estado' url="/"/>
+
+        <span className="text-sm text-zinc-500 pl-3 pt-4">Fundamentos</span>
+        <MenuItem icon={<IconBraces />} texto='JSX com JS' url="/fundamentos/soma"/>
+        <MenuItem icon={<IconFunction />} texto='JSX Chamando Funções' url="/fundamentos/funcoes"/>
+        <MenuItem icon={<IconFileCheck />} texto='Página #1' url="/fundamentos/pagina"/>
+        <MenuItem icon={<IconSitemap />} texto='Página #2' url="/pagina"/>
+        <span className="text-sm text-zinc-500 pl-3 pt-4">Componentes</span>
+        <MenuItem icon={<IconCode />} texto='Componente Basico' url="/components/basico"/>
+        <MenuItem icon={<IconH1 />} texto='Componente Título' url="/components/titulo"/>
     </div>
   )
 }
